1c03f129ff45ba7292ed5b72b561cd1396a0614c
Drew Crawford <thecodinghyrax@gmail.com>
2022-10-25 17:12:58 -0500

    Quick mock up from borrowed assets

---
+++ Adventure_Game_Final/mainwindow.cpp
@@ -0,0 +1,21 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    ui->stackedWidget->setCurrentIndex(1);
+}
+

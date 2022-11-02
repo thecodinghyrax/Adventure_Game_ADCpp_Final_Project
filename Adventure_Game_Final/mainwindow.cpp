684be99489b69db168ef1c186fec9cea5d0b00bb
thecodinghyrax <thecodinghyrax@gmail.com>
2022-11-02 13:36:46 -0500

    Created a few functions to update games scenes. Very rough atm.

--- Adventure_Game_Final/mainwindow.cpp
+++ Adventure_Game_Final/mainwindow.cpp
@@ -1,6 +1,8 @@
 ﻿#include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -16,8 +18,21 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->stackedWidget->setCurrentIndex(1);
+    createScene();
+    renderScene(scenes.at(0));
+    ui->stackedWidget->setCurrentIndex(1); // Index 1 will always be the location scene
 }
 
+void MainWindow::createScene(){ // This is tempary to test
+    Scene temp;
+    temp.setDescription("Dude, you have awoken in the middle of the night, startled by noise in the distance. You wait for a moment but there seems to be nothing there. Daylight is fast approaching. What is your command?");
+    temp.setImage(":/assests/bkg-camp.png");
+    temp.setSearchResult("You have search all around and found nothing");
+    scenes.push_back(temp);
+};
 
-
+void MainWindow::renderScene(Scene current){
+    QPixmap bkg(current.getImage().c_str());
+    ui->bkgLabel->setPixmap(bkg);
+    ui->descLabel->setText(current.getDescription().c_str());
+};

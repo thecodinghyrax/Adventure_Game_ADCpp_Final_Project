2686cde6db37cafb4f313c80f914785c1abdf1f4
thecodinghyrax <thecodinghyrax@gmail.com>
2022-11-03 14:53:09 -0500

    Added file loading for map

--- Adventure_Game_Final/mainwindow.cpp
+++ Adventure_Game_Final/mainwindow.cpp
@@ -16,23 +16,31 @@ MainWindow::~MainWindow()
 }
 
 
-void MainWindow::on_pushButton_clicked()
-{
-    createScene();
-    renderScene(areas.at(0));
-    ui->stackedWidget->setCurrentIndex(1); // Index 1 will always be the location scene
-}
+
 
 void MainWindow::createScene(){ // This is tempary to test
     Area temp;
+    temp.setId("0");
     temp.setText("Dude, you have awoken in the middle of the night, startled by noise in the distance. You wait for a moment but there seems to be nothing there. Daylight is fast approaching. What is your command?");
     temp.setBackgroundFile(":/assests/bkg-camp.png");
     temp.setSearchResult("You have search all around and found nothing");
-    areas.push_back(temp);
+    gameMap.setArea(temp);
 };
 
 void MainWindow::renderScene(Area current){
+    std::cout << current.getBackgroundFile() << std::endl;
+        std::cout << current.getId() << std::endl;
+            std::cout << current.getText() << std::endl;
+                std::cout << current.getName() << std::endl;
     QPixmap bkg(current.getBackgroundFile().c_str());
     ui->bkgLabel->setPixmap(bkg);
     ui->descLabel->setText(current.getText().c_str());
 };
+
+void MainWindow::on_beginButton_clicked()
+{
+    //createScene(gameMap.getAreaById("0")); /// Testing
+    renderScene(gameMap.getAreaById("0"));
+    ui->stackedWidget->setCurrentIndex(1); // Index 1 will always be the location scene
+}
+
--- Adventure_Game_Final/map.h
+++ Adventure_Game_Final/map.h
@@ -1,6 +1,13 @@
 ﻿#pragma once
+#include <string>
 #include <vector>
+#include <fstream>
+#include <iostream>
 #include "area.h"
+#include <QFile>
+#include <QTextStream>
+#include <QString>
+
 
 class Map
 {
@@ -8,11 +15,14 @@ private:
     // Vector containing the game's areas
     std::vector<Area> mapData;
 
-    Area getAreaById(std::string id);
+
     std::vector<Area> getMapData();
 public:
     Map();
     Map(std::vector<Area> areaVec);
 
     std::vector<Area> getConnectedAreas(Area workingArea);
+    Area getAreaById(std::string id);
+    void setArea(Area);
+    void loadAreasFromFile(std::string);
 };

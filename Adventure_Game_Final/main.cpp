e32d9840e15ce26e9736153407770e37c3663230
Drew Crawford <thecodinghyrax@gmail.com>
2022-11-10 13:17:04 -0600

    Added img and set size to fixed
    Added a proper image for the dump and made the window size fixed. Also added health and potions to ui

--- Adventure_Game_Final/main.cpp
+++ Adventure_Game_Final/main.cpp
@@ -1,10 +1,12 @@
-#include "mainwindow.h"
-
+﻿#include "mainwindow.h"
+#include <QFontDatabase>
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    QFontDatabase::addApplicationFont(":/assets/Planewalker.otf");
     MainWindow w;
     w.show();
     return a.exec();

de334c96a0040d20678ccd1e3404654bac04fc13
b-whisler <92903702+b-whisler@users.noreply.github.com>
2022-11-15 17:17:30 -0600

    Add item searching to Area class

--- Adventure_Game_Final/area.h
+++ Adventure_Game_Final/area.h
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include "enemy.h"
+#include "item.h"
 
 
 class Area
@@ -15,6 +16,10 @@ private:
     std::string text;
     // Text description of the results of searching area
     std::string searchResult;
+    // The area has been searched by the player
+    bool searched = false;
+    // The item to give the player when searching. Defaults to no item
+    Item searchItem = Item();
     // Filename of the area's background image
     std::string backgroundFile;
     // Vector of enemies that can be fought in the area
@@ -36,17 +41,22 @@ public:
     std::string getName(){return name;}
     std::string getText(){return text;}
     std::string getBackgroundFile(){return backgroundFile;}
-    std::string getSearchResult(){return searchResult;}
+    std::string getSearchResult();
+    bool hasBeenSearched(){return searched;}
+    Item getSearchItem(){return searchItem;}
     std::string getNorthArea(){return northArea;}
     std::string getEastArea(){return eastArea;}
     std::string getSouthArea(){return southArea;}
     std::string getWestArea(){return westArea;}
+
 
     void setId(std::string);
     void setName(std::string);
     void setText(std::string);
     void setBackgroundFile(std::string);
     void setSearchResult(std::string);
+    void setSearched(bool);
+    void setSearchItem(Item);
     void setNorthArea(std::string);
     void setEastArea(std::string);
     void setSouthArea(std::string);
